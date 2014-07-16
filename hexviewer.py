c9e4519852827b7a7f409bd39b9606a30c8010d8
cclauss <cclauss@bluewin.ch>
2014-07-16 01:11:59 +0100

    Removed code that was commented out

--- hexviewer.py
+++ hexviewer.py
@@ -10,50 +10,28 @@ def get_dir(path):
             dirs.append(entry)
         else:
             files.append(entry)
-    #dirs.sort()
-    #files.sort()
-    #all = []
-    #for dir in dirs:
-        #namelength = len(dir)
-        #all.append('{:89} | <{}>'.format('/' + dir, namelength))
     all = ['/' + dir for dir in dirs]
     for file in files:
-        #namelength = len(file)
         full_pathname = path + '/' + file
         size = '{} Bytes'.format(os.path.getsize(full_pathname))
         date = datetime.datetime.fromtimestamp(os.path.getmtime(full_pathname))
-        #all.append('{0:43} | {1:20} | {2:} | <{3:}>'.format(file, size, date, namelength))
         all.append('{:43} | {:20} | {}'.format(file, size, date))
     return all
 
 def table_tapped(sender):
     global path, tableview1
-    #global lst
-    #global tableview1
-    #global view
     rowtext = sender.items[sender.selected_row]
     filename_tapped = rowtext.partition('|')[0].strip()
-    #start, stop = rowtext.find('<'), rowtext.find('>')
-    #namelength = int(rowtext[start+1:stop])
-    if rowtext[0] == '/':
-        #if rowtext[:namelength+1] == '/..':
+     if rowtext[0] == '/':
         if filename_tapped == '/..':
             pos = path.rfind('/')
             path = path[:pos]
         else:
-            path = path + filename_tapped  # rowtext[:namelength+1]
+            path = path + filename_tapped
         all = get_dir(path)
         view.name = path
         tableview1.close()
         tableview1 = make_tableview1(view)
-        #tableview1 = ui.TableView()
-        #tableview1.frame = view.frame
-        #tableview1.x = tableview1.y = 0
-        #tableview1.flex = 'WH'
-        #tableview1.row_height = 30
-        #tableview1.background_color = '#DBDBDB'
-        #tableview1.allows_selection = True
-        #view.add_subview(tableview1)
         lst = ui.ListDataSource(all)
         tableview1.data_source = lst
         tableview1.delegate = lst
@@ -61,7 +39,7 @@ def table_tapped(sender):
         lst.action = table_tapped
         lst.delete_enabled = False
         return
-    filename = filename_tapped  # rowtext[:namelength]
+    filename = filename_tapped
     tableview1.hidden = True
     textview1 = ui.TextView()
     textview1.frame = view.frame
