c218240063ea8ed6e171f2615a3909e53088c78d
humberry <brummberry@gmail.com>
2014-08-06 20:24:03 +0200

    small search fix

--- hexviewer.py
+++ hexviewer.py
@@ -34,6 +34,9 @@ def hex_view(filepath):
     return return_value
 
 class HexViewerView(ui.View):
+    pos = -1
+    searchstr = ''
+
     def __init__(self):
         self.name = self.path = os.getcwd()
         self.background_color = 'white'
@@ -117,30 +120,30 @@ class HexViewerView(ui.View):
         return button
 
     def button_action(self, sender):
-        pos = -1
-        searchstr = ''
         tvd = self['tv_data']
         tfss = self['tf_search_str']
         if tfss.text != '':
-            if tfss.text == searchstr:
+            if tfss.text == HexViewerView.searchstr:
                 #next hit
-                pos = tvd.text.find(searchstr,pos+1)
+                HexViewerView.pos = tvd.text.find(HexViewerView.searchstr,HexViewerView.pos+1)
             else:
                 #new search
-                searchstr = tfss.text
-                pos = tvd.text.find(searchstr)
-            if pos >= 0:    #hit
-                x = tvd.text.find('\n',pos) - 79        #line start
+                HexViewerView.searchstr = tfss.text
+                HexViewerView.pos = tvd.text.find(HexViewerView.searchstr)
+            if HexViewerView.pos >= 0:    #hit
+                x = tvd.text.find('\n',HexViewerView.pos) - 79        #line start
                 y = len(tvd.text) - len(tvd.text) % 80  #last line start
-                if pos < y:
+                if HexViewerView.pos < y:
                     sender.title = tvd.text[x:x+10]
                 else:
                     sender.title = tvd.text[y:y+10]
-                tvd.selected_range = (pos, pos+len(searchstr))  # works only when textview is active!!!
+                tvd.selected_range = (HexViewerView.pos, HexViewerView.pos+len(HexViewerView.searchstr))  # works only when textview is active!!!
             else:
                 sender.title = 'Restart'
+                HexViewerView.pos = -1
         else:
             sender.title = 'Search'
+            HexViewerView.pos = -1
 
     def hexview_a_file(self, filename):
         self.tableview1.hidden = True
